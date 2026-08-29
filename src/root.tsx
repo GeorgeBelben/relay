c282a2c5ae06acb7f884aca9abc3ffbef9927b05
George Belben <george.belben@outlook.com>
2026-08-29 10:02:41 +0100

    chore: initial commit

---
+++ src/root.tsx
@@ -0,0 +1,10 @@
+import { Logo } from "./components/logo";
+
+export function App() {
+  return (
+    <div className="h-svh flex items-center justify-center">
+      <Logo className="w-32" />
+    </div>
+  );
+}
+
